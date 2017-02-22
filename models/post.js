0c99c5dc5eee0e835c4b8afb710434619c07f8ce
CLnagisa <437155119@qq.com>
2017-02-22 20:32:13 +0800

    标签和标签页完成

--- models/post.js
+++ models/post.js
@@ -1,9 +1,10 @@
 var mongodb = require('./db'),
 	markdown = require('markdown').markdown;
 
-function Post(name, title, post) {
+function Post(name, title, tags, post) {
 	this.name = name;
 	this.title = title;
+	this.tags = tags;
 	this.post = post;
 }
 
@@ -24,6 +25,7 @@ Post.prototype.save = function(callback) {
 	var post = {
         time: time,
         name: this.name,
+        tags: this.tags,
         post: this.post,
         title: this.title,
         comments: []
@@ -240,6 +242,61 @@ Post.getArchive = function(callback) {
 			}
 			//返回只包含name、time、title属性的文档组成的存档数组
 			collection.find({}, {
+				"name": 1,
+				"time": 1,
+				"title": 1
+			}).sort({
+				time: -1
+			}).toArray(function(err, docs) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null, docs);
+			});
+		});
+	});
+};
+
+//返回所有标签
+Post.getTags = function(callback) {
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+		db.collection('posts', function(err, docs) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+			//distinct用来找出给定键的所有不同值
+			collection.distinct('tags', function(err, docs) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null, docs);
+			});
+		});
+	});
+};
+
+//返回含有特定标签的所有文章
+Post.getTag = function(tag, callback) {
+	mongodb.open(function (err, db) {
+		if(err) {
+			return callback(err);
+		}
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return cllback(err);
+			}
+			//查询所有tags数组内包含tag的文档
+			//并返回只含有name、time、title组成的数组
+			collection.find({
+				"tags": tag
+			}, {
 				"name": 1,
 				"time": 1,
 				"title": 1
